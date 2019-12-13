bd8550b23d46acf57240e6d38fa38bff3b8b136f
lakshman_peethani <Lakshman_Peethani@epam.com>
2019-12-13 19:04:27 +0530

    Major refactoring of the code base to work with Core 3.1

--- src/BlazorRedux/ReduxComponent.cs
+++ src/BlazorRedux/ReduxComponent.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using Microsoft.AspNetCore.Blazor;
-using Microsoft.AspNetCore.Blazor.Components;
-using Microsoft.AspNetCore.Blazor.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace BlazorRedux
 {
-    public class ReduxComponent<TState, TAction> : BlazorComponent, IDisposable
+    public class ReduxComponent<TState, TAction> : ComponentBase, IDisposable
     {
         [Inject] public Store<TState, TAction> Store { get; set; }
-        [Inject] private IUriHelper UriHelper { get; set; }
+        [Inject] private NavigationManager UriHelper { get; set; }
 
         public TState State => Store.State;
 
@@ -19,7 +18,7 @@ namespace BlazorRedux
             Store.Change -= OnChangeHandler;
         }
 
-        protected override void OnInit()
+        protected override void OnInitialized()
         {
             Store.Init(UriHelper);
             Store.Change += OnChangeHandler;
@@ -30,6 +29,15 @@ namespace BlazorRedux
                 builder.OpenComponent<ReduxDevTools>(seq);
                 builder.CloseComponent();
             };
+            
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var seq = 0;
+            builder.OpenComponent<ReduxDevTools>(seq);
+            builder.CloseComponent();
         }
 
         private void OnChangeHandler(object sender, EventArgs e)
