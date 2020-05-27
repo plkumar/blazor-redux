b15ebb9c385ae0bfe476e6b0efc3b04bb2419f62
Lakshman Peethani <peethani@outlook.com>
2020-05-27 12:10:59 +0530

    Minor changes

--- samples/BlazorWasm.Core31/Pages/Counter.razor.cs
+++ samples/BlazorWasm.Core31/Pages/Counter.razor.cs
@@ -3,8 +3,6 @@ namespace BlazorWasm.Core31.Pages
 {
     public partial class Counter 
     {
-        private int currentCount = 0;
-
         private void IncrementCount()
         {
             Dispatch(new IncrementByValueAction(1));
